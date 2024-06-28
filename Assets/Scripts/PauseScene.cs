ca32668f6cfca57ad5d281835353315a50736ebf
Munteanu Alexandru <103954658+AlexandruMunteanuIoan@users.noreply.github.com>
2024-06-28 18:20:16 +0300

    Add pause scene and update background for menu scene

---
+++ Assets/Scripts/PauseScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScene : MonoBehaviour
+{
+    // Method to resume the game by reloading the GameScene
+    public void Play()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+
+    // Method to quit the game
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("The Player has Quit the game");
+    }
+}
